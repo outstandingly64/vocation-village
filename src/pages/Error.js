273a7bbf767a5a8568257e8da96d977906472779
Ivan T <112672027+outstandingly64@users.noreply.github.com>
2023-01-19 12:25:30 -0500

    Error Page Setup

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,6 +1,17 @@
+import {Link} from 'react-router-dom';
+import img from '../assets/images/not-found.svg';
+import Wrapper from '../assets/wrappers/ErrorPage';
+
 const Error = () => {
   return (
-    <h1>Error</h1>
+    <Wrapper className='full-page'>
+      <div>
+        <img src={img} alt="404 not found!" />
+        <h3>Page not found...</h3>
+        <p>The page you are looking for cannot be found.</p>
+        <Link to='/'>Back To Home</Link>
+      </div>
+    </Wrapper>
   )
 }
 
