67a4b22603157d811fa83f31f970c2b5255dbf76
Ivan T <112672027+outstandingly64@users.noreply.github.com>
2023-01-24 18:05:55 -0500

    nav bar initial structure setup

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,44 @@
 import Wrapper from "../assets/wrappers/Navbar";
-import { FaHome } from "react-icons/fa";
+import { FaAlignLeft, FaCaretDown, FaUserCircle } from "react-icons/fa";
+import Logo from "./Logo";
+import { useAppContext } from "../context/appContext";
 
 const NavBar = () => {
   return (
     <Wrapper>
-      <h3>NavBar</h3>
-      <FaHome />
+      <div className="nav-center">
+        <button
+          type="button"
+          className="toggle-btn"
+          onClick={() => console.log("sidebar toggle")}
+        >
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h3 className="logo-text">Dash</h3>
+        </div>
+        <div className="btn-container">
+          <button
+            type="button"
+            className="btn"
+            onClick={() => console.log("dropdown toggle")}
+          >
+            <FaUserCircle />
+            Ivan
+            <FaCaretDown />
+          </button>
+          <div className="dropdown show-dropdown">
+            <button
+              type="button"
+              className="dropdown-btn"
+              onClick={() => console.log("logout")}
+            >
+              Logout
+            </button>
+          </div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
