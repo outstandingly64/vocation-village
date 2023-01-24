95cb9eb89aa0e154e7ec7ab324171462099dd208
Ivan T <112672027+outstandingly64@users.noreply.github.com>
2023-01-24 18:25:43 -0500

    toggling logout dropdown css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,17 +2,15 @@ import Wrapper from "../assets/wrappers/Navbar";
 import { FaAlignLeft, FaCaretDown, FaUserCircle } from "react-icons/fa";
 import Logo from "./Logo";
 import { useAppContext } from "../context/appContext";
+import { useState } from "react";
 
 const NavBar = () => {
-    const {toggleSidebar} = useAppContext();
+  const [showLogout, setShowLogout] = useState(false);
+  const { toggleSidebar } = useAppContext();
   return (
     <Wrapper>
       <div className="nav-center">
-        <button
-          type="button"
-          className="toggle-btn"
-          onClick={toggleSidebar}
-        >
+        <button type="button" className="toggle-btn" onClick={toggleSidebar}>
           <FaAlignLeft />
         </button>
         <div>
@@ -23,13 +21,13 @@ const NavBar = () => {
           <button
             type="button"
             className="btn"
-            onClick={() => console.log("dropdown toggle")}
+            onClick={()=>setShowLogout(!showLogout)}
           >
             <FaUserCircle />
             Ivan
             <FaCaretDown />
           </button>
-          <div className="dropdown show-dropdown">
+          <div className={showLogout ? "dropdown show-dropdown" : "dropdown"}>
             <button
               type="button"
               className="dropdown-btn"
